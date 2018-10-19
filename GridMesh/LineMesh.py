851284ad3d9a8ad203759e2047fd4bfe9ec20e79
Zarhin <lizaixian1992@163.com>
2018-10-19 22:40:30 +0800

    第一次提交

--- GridMesh/LineMesh.py
+++ GridMesh/LineMesh.py
@@ -1,23 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import numpy as np 
+
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 x = np.linspace(0, 10, num=11, endpoint=True)
 y = np.cos(-x**2/9.0)
-f = interp1d(x,y)
+f = interp1d(x, y)
 f2 = interp1d(x, y, kind='cubic')
-print x,y,f(x)
+
+xnew = np.linspace(0, 10, num=41, endpoint=True)
+
+print y,f(xnew),f2(xnew)
+
+plt.plot(x, y, 'o', xnew, f(xnew), 'o', xnew, f2(xnew), 'o')
+plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.show()
 
 
 
 
-# 创建待插值的数据
-# x = np.linspace(0, 10, 11)
-# y = x
 
-# print (x,y)
+
+
+
+
+
+# import numpy as np 
+# from scipy.interpolate import interp1d
+
+# #创建待插值的数据
+# x = np.linspace(0, 10*np.pi, 20)
+# y = np.cos(x)
+
+# print("x=%f,y=%f" ,x,y)
 # a
 # # 分别用linear和quadratic插值
 # fl = interp1d(x, y, kind='linear')
@@ -35,4 +53,10 @@ print x,y,f(x)
 # pl.show()
 
 
+# input coordinate
+x1=0
+x2=10
+y1=0
+y2=10
 
+# node
